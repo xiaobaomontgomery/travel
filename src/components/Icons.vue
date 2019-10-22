82e61ef821aebdb5ed002b4086f6fa67739d5dff
xiaobaomontgomery <baohong.lu@hotmail.com>
2019-10-22 19:22:15 +0800

    实现图标区域分页滑动展示，标题过长用省略号显示

--- src/components/Icons.vue
+++ src/components/Icons.vue
@@ -1,11 +1,16 @@
 <template>
     <div class="icons">
-      <div class="icon" v-for="item of iconList" :key="item.id">
-          <div class="icon-img" >
-            <img class="icon-img-content" :src="item.imgUrl" alt="">
+      <swiper>
+        <swiper-slide v-for="(page, index) of pages" :key="index">
+          <div class="icon" v-for="item of page" :key="item.id">
+            <div class="icon-img" >
+              <img class="icon-img-content" :src="item.imgUrl" alt="">
+            </div>
+            <p class="icon-desc">{{item.desc}}</p>
           </div>
-        <p class="icon-desc">{{item.text}}</p>
-      </div>
+        </swiper-slide>
+      </swiper>
+
     </div>
 </template>
 
@@ -17,44 +22,61 @@
         iconList: [{
           id: '001',
           imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
-          text: '热门景点'
+          desc: '热门景点'
         }, {
           id: '002',
           imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/94f7f5bf7b0f4d238dd68d718f102f40.png',
-          text: '中山陵'
+          desc: '中山陵'
         }, {
           id: '003',
           imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/75/eca3ce656c886502.png',
-          text: '秋意浓'
+          desc: '秋意浓'
         }, {
           id: '004',
           imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/97/02f5043b51b2102.png',
-          text: '牛首山'
+          desc: '牛首山'
         }, {
           id: '005',
           imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
-          text: '一日游'
+          desc: '一日游'
         }, {
           id: '006',
           imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/b71c1387a921ccf9c6edc7e8def3da90.png',
-          text: '红山森林动物园'
+          desc: '红山森林动物园'
         }, {
           id: '007',
           imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/f6dda1876c5950d83cc159c8fd522943.png',
-          text: '总统府'
+          desc: '总统府'
         }, {
           id: '008',
           imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png',
-          text: '银杏湖乐园'
+          desc: '银杏湖乐园'
+        }, {
+          id: '009',
+          imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png',
+          desc: '大报恩寺'
         }] };
+    },
+    computed: {
+      pages() {
+        const pages = [];
+        this.iconList.forEach((item, index) => {
+          const page = Math.floor(index / 8);
+          if (!pages[page]) {
+            pages[page] = [];
+          }
+          pages[page].push(item);
+        });
+        return pages;
+      }
     }
   };
 </script>
 
 <style lang="stylus" scoped>
   @import "~@/assets/styles/variable.styl";
-  .icons{
-    overflow: hidden;
+  @import "~@/assets/styles/mixins.styl";
+  .icons >>> .swiper-container{
     height: 0;
     padding-bottom: 50%;
   }
@@ -89,6 +111,7 @@
     line-height: 1.32rem;
     color: $darkTextColor;
     text-align: center;
+    ellipsis();
   }
 
 </style>
