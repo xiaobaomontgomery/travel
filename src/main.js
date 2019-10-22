540ae33da4c83a35b1e0c79db37951ff7f7edd4c
xiaobaomontgomery <baohong.lu@hotmail.com>
2019-10-22 08:24:13 +0800

    引入font-awesome

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import fastClick from 'fastclick'
-import './assets/styles/reset.css'
-import './assets/styles/border.css'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import fastClick from 'fastclick';
+import 'font-awesome/css/font-awesome.min.css';
+import './assets/styles/reset.css';
+import './assets/styles/border.css';
 
-Vue.config.productionTip = false
-fastClick.attach(document.body)
+Vue.config.productionTip = false;
+fastClick.attach(document.body);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
