154f041cc08d05cdeb982f1e131bdabe44cb4e58
xiaobaomontgomery <baohong.lu@hotmail.com>
2019-10-23 09:09:38 +0800

    实现热门推荐区域展示

---
+++ src/components/Recommend.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="recommend-title">
+    <div>
+      热销推荐
+    </div>
+    <ul>
+      <li class="item" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" alt="">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeRecommend',
+    data() {
+      return {
+        recommendList: [{
+          id: '0001',
+          imgUrl: 'https://imgs.qunarzz.com/sight/p18/201211/03/db9f2bb27ee40bf893835fbb.jpg_200x200_cf1de8d6.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首战 浪漫的海洋主题公园'
+        }, {
+          id: '0002',
+          imgUrl: 'https://imgs.qunarzz.com/sight/p18/201211/03/db9f2bb27ee40bf893835fbb.jpg_200x200_cf1de8d6.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首战 浪漫的海洋主题公园'
+        }, {
+          id: '0003',
+          imgUrl: 'https://imgs.qunarzz.com/sight/p18/201211/03/db9f2bb27ee40bf893835fbb.jpg_200x200_cf1de8d6.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首战 浪漫的海洋主题公园'
+        }]
+      };
+    }
+  };
+</script>
+
+<style lang="stylus" scoped>
+  @import "~@/assets/styles/mixins.styl";
+  .recommend-title{
+    margin-top :0.6rem;
+    line-height :2.4rem;
+    background :#eee;
+    text-indent :0.6rem;
+  }
+  .item{
+    overflow: hidden;
+    display: flex;
+    height: 5.7rem;
+    background:#fff;
+  }
+  .item-img{
+    width: 5.4rem;
+    height: 5.4rem;
+    padding: 0.3rem;
+  }
+  .item-info{
+    flex: 1;
+    padding: 0.3rem;
+    min-width: 0;
+  }
+  .item-title{
+    line-height: 1.62rem;
+    font-size: 0.96rem;
+    ellipsis();
+  }
+  .item-desc{
+    line-height: 1.2rem;
+    color:#ccc;
+    ellipsis();
+  }
+  .item-button{
+    line-height: 1.32rem;
+    margin-top: 0.48rem;
+    background: #ff9300;
+    padding: 0 0.6rem;
+    border-radius: 0.18rem;
+    color: #fff;
+  }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-recommend></home-recommend>
   </div>
 </template>
 
@@ -11,12 +12,14 @@
   import HomeHeader from '@/components/Header.vue';
   import HomeSwiper from '@/components/Swiper';
   import HomeIcons from '@/components/Icons';
+  import HomeRecommend from '@/components/Recommend';
   export default {
     name: 'home',
     components: {
       HomeHeader,
       HomeSwiper,
-      HomeIcons
+      HomeIcons,
+      HomeRecommend
     }
   };
 </script>
