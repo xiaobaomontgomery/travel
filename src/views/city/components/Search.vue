ab60de1621398d977fcdb9a4623ddf812afac275
xiaobaomontgomery <baohong.lu@hotmail.com>
2019-10-26 11:20:36 +0800

    实现搜索框功能

---
+++ src/views/city/components/Search.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="search">
+      <input class="search-input" type="text" placeholder="输入城市名或拼音">
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'CitySearch'
+  };
+</script>
+
+<style lang="stylus" scoped>
+  @import "~@/assets/styles/variable.styl"
+  .search{
+    height: 2.16rem;
+    padding: 0.5rem;
+    background: $bgColor;
+  }
+  .search-input{
+    box-sizing: border-box;
+    padding: 0 0.3rem;
+    height: 1.86rem;
+    line-height: 1.86rem;
+    width: 99%;
+    border-radius: 0.2rem;
+    border-width 0;
+    text-align:center;
+    color: #666;
+  }
+
+</style>
