573fb03b25e9901aee26846185c4bfe51d41dd23
xiaobaomontgomery <baohong.lu@hotmail.com>
2019-10-26 19:31:57 +0800

    better-scroll的使用及字母表布局

---
+++ src/views/city/components/Alphabet.vue
@@ -0,0 +1,36 @@
+<template>
+    <ul class="list">
+      <li class="item">A</li>
+      <li class="item">B</li>
+      <li class="item">C</li>
+      <li class="item">D</li>
+      <li class="item">E</li>
+      <li class="item">F</li>
+    </ul>
+</template>
+
+<script>
+  export default {
+    name: 'CityAlphabet'
+  };
+</script>
+
+<style lang="stylus" scoped>
+  @import "~@/assets/styles/variable.styl";
+  .list{
+    display: flex;
+    flex-direction: column;
+    justify-content:center;
+    position: absolute;
+    top:5.8rem;
+    right:0;
+    bottom:0;
+    width:1.2rem;
+  }
+  .item{
+    line-height:1.2rem;
+    text-align:center;
+    color: $bgColor;
+  }
+
+</style>
