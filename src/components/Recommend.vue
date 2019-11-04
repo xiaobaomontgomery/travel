303fdb2cee08edaa3d1d8d14c74875993c34d5ca
xiaobaomontgomery <baohong.lu@hotmail.com>
2019-11-04 19:16:03 +0800

    热销推荐里详情页面图片部分样式设置

--- src/components/Recommend.vue
+++ src/components/Recommend.vue
@@ -4,16 +4,20 @@
       热销推荐
     </div>
     <ul>
-      <li class="item" v-for="item of list" :key="item.id">
-        <div class="item-img-wrapper">
+      <router-link
+        tag="li"
+        class="item"
+        v-for="item of list"
+        :key="item.id"
+        :to="'/detail/' + item.id"
+      >
           <img class="item-img" :src="item.imgUrl" alt="">
-        </div>
-        <div class="item-info">
-          <p class="item-title">{{item.title}}</p>
-          <p class="item-desc">{{item.desc}}</p>
-          <button class="item-button">查看详情</button>
-        </div>
-      </li>
+          <div class="item-info">
+            <p class="item-title">{{item.title}}</p>
+            <p class="item-desc">{{item.desc}}</p>
+            <button class="item-button">查看详情</button>
+          </div>
+      </router-link>
     </ul>
   </div>
 </template>
